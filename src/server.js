2885f1133df8b7c9b4fb0f9fcef78a946720aa98
Much. Iqbal Fauzi <672018382@student.uksw.edu>
2022-08-31 02:09:57 +0700

    auth

--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ import express from "express";
 import { json, urlencoded } from "body-parser";
 import morgan from "morgan";
 import cors from "cors";
-import itemRouter from './resources/item/item.router'
+import config from "./config";
+import { signup, signin, protect } from "./utils/auth";
+import { connect } from "./utils/db";
+import userRouter from './resources/user/user.router';
+import itemRouter from './resources/item/item.router';
+import listRouter from './resources/list/list.router';
 
 export const app = express()
-const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -14,46 +18,21 @@ app.use(json())
 app.use(urlencoded())
 app.use(morgan('dev'))
 
+app.post('/signup', signup)
+app.post('/signin', signin)
+
+app.use('/api', protect)
+app.use('/api/user'. userRouter)
 app.use('/api/item', itemRouter)
-// const log = (req, res, next) => {
-//     console.log('Logging')
-//     next()
-// }
-
-// app.use(log)
-
-// app.get('/data', (req, res) => {
-//     res.send({message: "hello"})
-// })
-
-// app.post('/data', (req, res) => {
-//     console.log(req.body)
-//     res.send({...req.body,message: "Ok"})
-// })
-
-// app.get('^(me)', (req, res) => {
-//     res.send({message: "your regex example, other /* :id "})
-// })
-
-// const routes = ['get /cat', 'get /cat/:id', 'post /cat', 'put /cat/:id', 'delete /cat/:id']
-
-// router.route('/cat')
-//     .get()
-//     .post()
-
-// router.route('/cat/:id')
-//     .get()
-//     .put()
-//     .delete()
-
-router.get('me', (req, res) => {
-    res.send({me: 'hello'})
-})
-
-app.use('/api', router)
+app.use('/api/list'. listRouter)
 
 export const start = () => {
-    app.listen(3000, () => {
-        console.log("server is on port 3000")
-    })
+    try {
+        await connect()
+        app.listen(config.port, () => {
+            console.log(`REST API on http://localhost:${config.port}/api`)
+        })
+    } catch (err) {
+        console.error(err)
+    }
 }
